c7b8227f61a5f8b51570fe4dcf9137186e4d88e0
Matheus Batista <matheus.vbatista04@gmail.com>
2023-07-27 15:57:14 -0300

    chore: auth0 root setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { env } from './env-handler';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={env.REACT_APP_DOMAIN}
+    clientId={env.REACT_APP_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: env.REACT_APP_AUD,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
